9069a8a623e8978e65322d36cc1a12808b9cbacd
Gerolineto <gabrielgerolineto@gmail.com>
2022-03-24 07:40:49 -0300

    Implementação do CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Teste Projeto</h1>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input 
+        type="text"
+        placeholder="Digite seu cep..."
+        />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+      </div>
+
+      <main className='main'>
+        <h2>CEP: 38408250</h2>
+        <span>Rua Teste Algum</span>
+        <span>Complemento: algum </span>
+        <span>Vila Rosa</span>
+        <span>Campo Grande -MS </span>
+        <span>Brasil</span>
+
+      </main>
+
     </div>
   );
 }
